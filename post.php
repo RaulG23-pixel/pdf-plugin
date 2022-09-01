a2ffa2e40ea53338b7cfb79a7d5f90cf34d8105b
Raul Gonzalez <raulx12@gmail.com>
2022-09-01 11:03:24 -0400

    incorporacion de composer al proyecto

--- post.php
+++ post.php
@@ -1,30 +1,30 @@
 <?php 
 
 //Importacion de DOMPDF
-require __DIR__ ."/src/dompdf_nightly/dompdf/autoload.inc.php";
+require __DIR__ "./vendor/autoload.php";
 
 use Dompdf\Dompdf;
 use Dompdf\Exception;
 use Dompdf\Options;
 
 //Lectura del archivo
-$filename = __DIR__ . "/template.html";
+$filename = __DIR__ . "/template.php";
 
 $file = fopen($filename,'r');
 $text = fread($file, filesize($filename));
 fclose($file);
 
-$options = new Options();
+/*$options = new Options();
 $options->set('isRemoteEnabled',true);      
 $dompdf = new Dompdf( $options );
-$dompdf->getOptions()->setChroot("C:\\MAMP\\htdocs\\pruebas");
 
 $dompdf->loadHtml($text);
 $dompdf->setPaper("letter");
 
 
 $dompdf->render();
-$dompdf->stream("document.pdf");
+$dompdf->stream("document.pdf");*/
+echo $text;
 
 ?>
 
